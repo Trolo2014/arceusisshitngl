28378b77e82a17e071330212b5e5a4ca1efe5a2a
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-07-15 00:38:17 +0200

    Update Whitelister (obf).lua

--- Whitelister (obf).lua
+++ Whitelister (obf).lua
@@ -1,6 +1,1 @@
-local Whitelist = {"17482CEE-6B9B-4197-83E2-CE710986F1A8"}
-if table.find(Whitelist, game:GetService("RbxAnalyticsService"):GetClientId()) then
-while task.wait() do
-game:GetService("TeleportService"):Teleport(5373028495, game:GetService("Players").LocalPlayer)
-end
-end
+
